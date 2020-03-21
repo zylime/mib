501275af17b767da2a351d615dff974b95133fc3
HM <z.ylime@hotmail.com>
2020-03-21 17:33:36 +0800

    updated page 66 with new design

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -156,7 +156,10 @@ $.fn.AddProduct = function (opts) {
   var form = container.find('form');
   var submitBtn = container.find('.js-submit');
   var addNewProductBtn = container.find('.js-add-new-product');
-
+  var promtContainer = container.find('.js-promt-container');
+  var categoryContainer = container.find('.js-category-container');
+  var promtHtml = container.find('.js-promt-container').html();
+  var categoryHtml = container.find('.js-category-container').html();
   var productHtml = container.find('.js-product-container').html();
 
   events();
@@ -176,9 +179,25 @@ $.fn.AddProduct = function (opts) {
     $(document).on('click touch', '.js-collapse-btn', function () {
       $(this).parent('.collapse').toggleClass('active');
     });
+    // +-额外优惠
+    $(document).on('click touch', '.js-add-promt', function () {
+      addPromt();
+    });
+    $(document).on('click touch', '.js-remove-promt', function () {
+      $(this).parents('.js-promt-item').remove();
+    });
+
+    // +-商品类别
+    $(document).on('click touch', '.js-add-category', function () {
+      addCategory();
+    });
+    $(document).on('click touch', '.js-remove-category', function () {
+      $(this).parents('.js-category-item').remove();
+    });
   }
   function initAll() {
     radioToggle();
+    invoiceTypeToggle();
     checkboxToggle();
     removeItem();
     submitBtn.on('click touch', function () {
@@ -220,6 +239,14 @@ $.fn.AddProduct = function (opts) {
     });
   }
 
+  function invoiceTypeToggle() {
+    $('.js-invoice-radio-group .js-invoice-radio-btn').on('click touch', function () {
+      if (!$(this).hasClass('active')) {
+        $(this).parents('.js-invoice-radio-group').find('.js-invoice-radio-btn').toggleClass('active');
+      }
+    });
+  }
+
   function checkboxToggle() {
     container.find('.js-checkbox').on('click touch', function () {
       $(this).toggleClass('active');
@@ -231,6 +258,18 @@ $.fn.AddProduct = function (opts) {
       $(this).parents('.c-guide__add-product__list').remove();
       addNewProductBtn.show();
     });
+  }
+
+  function addPromt() {
+    $(promtContainer).find('.js-remove-promt').removeClass('hide');
+    $(promtContainer).find('.js-add-promt').addClass('hide');
+    $(promtHtml).appendTo(promtContainer);
+  }
+
+  function addCategory() {
+    $(categoryContainer).find('.js-remove-category').removeClass('hide');
+    $(categoryContainer).find('.js-add-category').addClass('hide');
+    $(categoryHtml).appendTo(categoryContainer);
   }
 };
 'use strict';
